adede6de44b44c34fc0804833551d8304d95ac6b
Victor Loyola <victor.tostes@gmail.com>
2021-08-27 13:28:54 -0400

    Added Dashboard and Summary components

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Summary } from '../Summary';
+import { Container } from './styles';
+
+export function Dasboard() {
+  return(
+    <Container>
+      <Summary />
+    </Container>
+  )
+}

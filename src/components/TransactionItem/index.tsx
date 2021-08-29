020319403738fdab8d9b2ecc397fe7703a9d145b
Victor Loyola <victor.tostes@gmail.com>
2021-08-28 20:16:40 -0400

    Formatted amount and date to pt-BR format

--- src/components/TransactionItem/index.tsx
+++ src/components/TransactionItem/index.tsx
@@ -8,9 +8,17 @@ export function TransactionItem({ transaction }: TransactionItemProps) {
   return (
     <tr>
       <td>{transaction.title}</td>
-      <td className={transaction.transactionType}>{transaction.amount}</td>
+      <td className={transaction.transactionType}>
+        {new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(transaction.amount)}
+      </td>
       <td>{transaction.category}</td>
-      <td>{transaction.createdAt}</td>
+      <td>
+        {new Intl.DateTimeFormat('pt-BR')
+          .format(new Date(transaction.createdAt))}
+      </td>
     </tr>
   )
 }
